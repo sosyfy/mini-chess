efc2b4468e299b11bd3d1a51066b8b364ad9090e
Sospeter Karanja <sospeterkaranja02@gmail.com>
2023-07-03 16:18:08 +0300

    fixed jotai swc error

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   // @ts-ignore
-  plugins: [react(), ["@swc-jotai/react-refresh", {}]],
+  plugins: [react()],
 })
